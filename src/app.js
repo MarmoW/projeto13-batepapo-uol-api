28078ae9fc790258c709c8dd1abc424f51786347
MarmoW <115847012+MarmoW@users.noreply.github.com>
2023-01-15 20:51:33 -0300

    Small Changes

--- src/app.js
+++ src/app.js
@@ -21,14 +21,16 @@ server.post("/participants", async (req, res) => {
         name: joi.string().required()
     })
     const validation = usuarioSchema.validate(usuario)
+
     console.log(validation)
+
     try{
-        const userJaCadastrado = await db.collect("participants").findOne({usuario})
+        const userJaCadastrado = await db.collection("participants").findOne({usuario})
         
         if(userJaCadastrado) return res.status(409).send("Já existe um usuário com esse nome")
-        const cadastrar = await db.collect("participants").insertOne({name: usuario, lastStatus: Date.now()})
+        const cadastrar = await db.collection("participants").insertOne({name: usuario, lastStatus: Date.now()})
         
-        const conectou = await db.collect("messages").insertOne({name: usuario, to:"Todos", text:"entra na sala...", type:"status",  time: dayjs("HH:mm:ss")})
+        const conectou = await db.collection("messages").insertOne({name: usuario, to:"Todos", text:"entra na sala...", type:"status",  time: dayjs("HH:mm:ss")})
         
         res.send("usuário cadastrado")
         
@@ -49,12 +51,36 @@ server.get("/participants", async (req, res) => {
 
 })
 
-server.post("/mensages", (req, res) => {
-    
+server.post("/mensages", async (req, res) => {
+    let mensagens = req.data
+    let userheader = req.header.user
+    console.log(userheader)
+
+    const mensageSchema = joi.object({
+        to: joi.string().required(),
+        text: joi.string().required(),
+        type: joi.string().valid("message", "private_message").required()
+     })
+
+    const validate = mensageSchema.validate(mensagens)
+
+    try{
+        const userOnline = await db.collection("participantes").findOne({userheader})
+        if(!userOnline) return res.send("Você foi desconectado")
+        const mensagemEnviada = await db.collection("mensages").insertOne({from: userheader,to: mensagens.to ,text: mensagens.text ,type: mensagens.type ,time: dayjs("HH:mm:ss")})
+
+    }catch(err){
+        res.status(422)
+        console.log(err)
+    }
+     
+
+
 })
 
 server.get("/mensages", (req, res) => {
-    
+    let userheader = req.header.user
+    let limitMensage = req.query
 })
 
 server.post("/status", (req, res) => {
