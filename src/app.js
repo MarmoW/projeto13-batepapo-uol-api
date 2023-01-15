1e6d3e62fbdb2e650182ed59264abe0779443730
MarmoW <115847012+MarmoW@users.noreply.github.com>
2023-01-15 19:32:29 -0300

    post mensages

--- src/app.js
+++ src/app.js
@@ -2,10 +2,12 @@ import express from "express"
 import cors from "cors"
 import { MongoClient } from "mongodb"
 import dotenv from "dotenv"
+import dayjs from "dayjs"
+import joi from "joi"
 
 dotenv.config()
 
-const mongoClient = new MongoClient(process.send.DATABASE_URL)
+const mongoClient = new MongoClient("mongodb://localhost:27017")
 let db
 
 
@@ -18,33 +20,40 @@ server.post("/participants", async (req, res) => {
     const usuarioSchema = joi.object({
         name: joi.string().required()
     })
-    const validation = usuarioSchema.validade(usuario)
-
+    const validation = usuarioSchema.validate(usuario)
+    console.log(validation)
     try{
         const userJaCadastrado = await db.collect("participants").findOne({usuario})
         
         if(userJaCadastrado) return res.status(409).send("Já existe um usuário com esse nome")
-
         const cadastrar = await db.collect("participants").insertOne({name: usuario, lastStatus: Date.now()})
-
-        const conectou = await db.collect("messages").insertOne({name: usuario, to:"Todos", text:"entra na sala...", type:"status",  time: "HH:MM:SS"})
-
+        
+        const conectou = await db.collect("messages").insertOne({name: usuario, to:"Todos", text:"entra na sala...", type:"status",  time: dayjs("HH:mm:ss")})
+        
         res.send("usuário cadastrado")
-
+        
     }catch(err){
         res.status(422)
     }
 })
 
-server.get("/participants", (req, res) => {
-    
+server.get("/participants", async (req, res) => {
+    try{
+        const listaParticipantes = await db.collect("participantes").find()
+        return listaParticipantes
+
+    }catch(err){
+        console.log(err)
+        res.send(err)
+    }
+
 })
 
 server.post("/mensages", (req, res) => {
     
 })
 
-server.post("/mensages", (req, res) => {
+server.get("/mensages", (req, res) => {
     
 })
 
