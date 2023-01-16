a34b22a0f720e3b80204710e17597e658b2bd47e
MarmoW <115847012+MarmoW@users.noreply.github.com>
2023-01-16 01:32:05 -0300

    fixed

--- src/app.js
+++ src/app.js
@@ -57,8 +57,13 @@ server.post("/messages", async (req, res) => {
     const {to, text, type} = req.body
     const {user} = req.headers
     let mensagem = req.body
-    console.log(user)
+    
     if(user === undefined) return res.sendStatus(422)
+    
+    const userCadastrado = await db.colletion("participants").findOne({name: user})
+
+    if(!userCadastrado) return res.sendStatus(422)
+
     const messageSchema = joi.object({
         to: joi.string().required(),
         text: joi.string().required(),
